f6500f3dc2332225f8b3df59f7cae24d95e4f4a8
Lars Van Cauter <larsvancauter@gmail.com>
2021-06-24 13:02:49 +0200

    cli options

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 
 
 def parseLine(line):
@@ -10,47 +11,51 @@ def parseLine(line):
     return res
 
 
-def convert(file):
+def convert(file, output, author, ct, nonums):
+    sec = "section"
+    if nonums:
+        sec = "section*"
     pdf = ""
     pdf += "\\documentclass[a4paper,12pt]{article}\n"
 
     # packages
     pdf += "\\usepackage[normalem]{ulem}\n"
     pdf += "\\usepackage{hyperref}\n"
-    pdf += "\\hypersetup{colorlinks=true, linkcolor=blue, filecolor=magenta, urlcolor=cyan, pdfpagemode=FullScreen}"
-    pdf += "\\urlstyle{same}"
+    pdf += "\\hypersetup{colorlinks=true, linkcolor=blue, filecolor=magenta, urlcolor=cyan, pdfpagemode=FullScreen}\n"
+    pdf += "\\urlstyle{same}\n"
 
+    if author != "":
+        pdf += "\\author{"+author+"}\n"
     pdf += "\\begin{document}\n"
     title = ""
     aftertext = ""
     with open(file, "r") as f:
         currentListDepth = -1
-        currentList = []
         line = f.readline()
         while line != "":
             if not re.match(r"^ *[*-+] .*$", line) and currentListDepth != -1:
                 while currentListDepth != -1:
-                    aftertext += "\\end{itemize}\n" 
+                    aftertext += "\\end{itemize}\n"
                     currentListDepth -= 1
 
             if re.match(r"^# .*$", line):
                 title = line[2:-1]
             elif re.match(r"^## .*$", line):
-                aftertext += "\section{" + line[3:-1] + "}\n"
+                aftertext += "\\" + sec + "{" + line[3:-1] + "}\n"
             elif re.match(r"^### .*$", line):
-                aftertext += "\subsection{" + line[4:-1] + "}\n"
+                aftertext += "\sub" + sec + "{" + line[4:-1] + "}\n"
             elif re.match(r"^#### .*$", line):
-                aftertext += "\subsubsection{" + line[5:-1] + "}\n"
+                aftertext += "\subsub" + sec + "{" + line[5:-1] + "}\n"
             elif re.match(r"^ *[*-+] .*$", line):
                 spaces = len(line) - len(line.lstrip(' '))
                 if spaces/2 > currentListDepth:
                     aftertext += "\\begin{itemize}\n"
                     currentListDepth += 1
                 elif spaces/2 < currentListDepth:
-                    aftertext += "\\end{itemize}\n" 
+                    aftertext += "\\end{itemize}\n"
                     currentListDepth -= 1
 
-                aftertext += "\\item "+ line[spaces+2:]
+                aftertext += "\\item " + line[spaces+2:]
 
             else:
                 aftertext += parseLine(line) + " \\\\ \n"
@@ -58,11 +63,24 @@ def convert(file):
             line = f.readline()
 
     # writeback
-    pdf += "\\title{"+title+"}\n\\maketitle\n\\newpage\n"
+    pdf += "\\title{"+title+"}\n\\maketitle\n"
+    if ct:
+        pdf += "\\tableofcontents"
+    pdf += "\\newpage\n"
     pdf += aftertext
     pdf += "\\end{document}"
-    with open("output.tex", "w") as f:
+    with open(output, "w") as f:
         f.write(pdf)
 
 
-convert("test.md")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--contenttable",
+                        action="store_true", help="Enable a content table")
+    parser.add_argument("-a", "--author", type=str, default="")
+    parser.add_argument("-i", "--input", type=str, required=True)
+    parser.add_argument("-o", "--output", type=str, default="output.tex")
+    parser.add_argument("--nonums", action="store_true")
+    args = parser.parse_args()
+    convert(args.input, args.output, args.author,
+            args.contenttable, args.nonums)
